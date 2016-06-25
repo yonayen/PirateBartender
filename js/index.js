0c90d503774ff99a7e7cf7a39d9413468e31b958
Yonathan Ayenew <yonayen@gmail.com>
2016-06-25 16:56:47 -0700

    add function to display and update the title

--- js/index.js
+++ js/index.js
@@ -1,3 +1,29 @@
 $(document).ready(function() {
 	loadQuestions(); // Questions start at pageLoad.
 });
+
+// var options array from JSON file houses Questions key-value pairs, Answers array and the correct answer]
+
+var options = [];
+
+function loadQuestions() {
+	$.getJSON("ingredients.json", function (data){
+		options = data.options
+		restartQuestions();
+	});
+}
+
+// global variables
+
+var currentQuestion = 0; // Starts with first question in var options
+
+// begin again
+function restartQuestions()
+{
+	currentQuestion = 0; // reset to zero
+}
+
+function updateQuestionTitle(title)
+{
+	$(".question-title").text(title);
+}
