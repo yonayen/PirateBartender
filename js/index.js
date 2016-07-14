8149333c128ddff5e59b49d3ff5013cfc6fb9bd7
Yonathan Ayenew <yonayen@gmail.com>
2016-07-13 17:52:56 -0700

    append final drink to container

--- js/index.js
+++ js/index.js
@@ -24,8 +24,15 @@ function showDrink () {
 	// instance of bartender and passing 'create drink' method, passing preferences object -- will return drink to use new drink = bartender.createDrink
 	var newDrink = bartender.createDrink(user_preferences);
 
-	
-	// Use jQuery to show this on the front end (last page)
+	// Use jQuery to display final drink
+	$('#main-container').append( function () {
+		[
+		 '<h2 class="question-title">',
+		 'You chose ',
+		  newDrink,
+		  '</h2>',
+		].join('');
+	});
 }
 
 
@@ -42,99 +49,6 @@ $('#main-container').on('click', '.no', function () {
 	currentQuestionIndex++;
 	showQuestion();
 });
-
-
-
-
-
-/*
-
-// jQuery Functionality 
-
-$(document).ready(function() {
-	loadQuestions(); // Questions start at pageLoad.
-});
-
-// var options array from JSON file houses Questions key-value pairs, Answers array and the correct answer]
-
-var options = [];
-
-function loadQuestions() {
-	$.getJSON("ingredients.json", function (data){
-		options = data.options
-		restartQuestions();
-	});
-}
-
-// global variables
-
-var currentQuestion = 0; // Starts with first question in var options
-
-// begin again
-function restartQuestions()
-{
-	currentQuestion = 0; // reset to zero
-}
-
-function updateQuestionTitle(title)
-{
-	$(".question-title").text(title);
-}
-
-function updateQuestionOptions(optionsArray,correct)
-{
-	for(var i=0; i<optionsArray.length;i++)
-	{
-		//["Shake of bitters", "splash of tonic", "twist of lemon peel"],
-		//       0                    1                    2
-
-		if()
-		{
-			
-		}
-		$(".question-options").append('<li class=' + '</li>');
-	}
-}
-
-function nextQuestion()
-{
-	currentQuestion++;
-
-	if (currentQuestion < options.length)
-	{
-		showNextQuestion();
-	}
-	else
-	{
-		showFinalDrink();
-
-	}
-}
-
-function showNextQuestion()
-{
-	$(".question-options").empty();
-
-	updateQuestionTitle(options[currentQuestion].question);
-	updateQuestionOptions(options[currentQuestion].answers, options[currentQuestion].type);
-}
-
-function showFinalDrink()
-{
-	$(".question-title").text("Here's Your Drink!");
-
-	$(".question-options").one("click", ".answer", function() {
-		loadQuestions();
-	});
-}
-
-/*
- 1- Bartender asks questions (yes or no)
- 2- If yes, save the type and select (randomly) one of the ingredients
- 3- 
-*/
-
-
 
 
 
