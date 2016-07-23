726fef937e11a05b07288cd7b2a7ef41128b818b
Yonathan Ayenew <yonayen@gmail.com>
2016-07-22 18:18:58 -0700

    Add currentQuestion type to preferences object

--- js/index.js
+++ js/index.js
@@ -43,6 +43,7 @@ $('#main-container').on('click', '.yes', function () {
 	var question = questionsJson[currentQuestionIndex]
 
 	for (var i = 0; i < questionsJson.length; i++) {
+		selected_preference.push(question.type);
 		// Add currentQuestion type to preferences object
 	}
 
